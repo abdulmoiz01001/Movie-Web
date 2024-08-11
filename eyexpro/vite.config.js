667433cb6289686356d3a3ca6dc618000d4a1435
abdulmoiz01001 <profession.ai01001@hotmail.com>
2024-08-11 15:59:53 +0500

    donee

--- eyexpro/vite.config.js
+++ eyexpro/vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:"/",
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://movie-web-api-pink.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
